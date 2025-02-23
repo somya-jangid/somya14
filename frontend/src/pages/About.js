2695e8fbdca47766629baa24ce8e64bf6b35c8dd
somya-jangid <somyajangid78@gmail.com>
2025-02-23 23:46:59 +0530

    Add or update frontend/src/pages/About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './About.css';
+
+function About() {
+  return (
+    <div className="about">
+      <h1>About Me</h1>
+      <p>This is the about page where you can learn more about me.</p>
+      <img src="https://www.pexels.com/photo/man-wearing-black-suit-jacket-1222271/" alt="About" />
+    </div>
+  );
+}
+
+export default About;
